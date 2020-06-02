cbc0d9b8ac2d4aa74eaf0c82ed51229e76b8ee8d
root <root@ip-172-31-24-21.us-east-2.compute.internal>
2020-06-02 17:40:24 +0000

    welcome

--- src/main/java/ToolsQA/App.java
+++ src/main/java/ToolsQA/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Harinath!" );
-	System.out.println("hello world");
+	System.out.println("Welcome");
     }
 }
